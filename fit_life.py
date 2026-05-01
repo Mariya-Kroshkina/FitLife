d916c85ccf757a616eeb3e596acaca6433dd7a09
mariya_kroshkina <mariya170521@gmail.com>
2026-05-01 15:04:54 +0300

    Вынесла константы в отдельный файл, добавила обработку ошибок

--- fit_life.py
+++ fit_life.py
@@ -1,23 +1,28 @@
 # Проект FitLife - MVP версия 1.0
-WATER_PER_KG = 30
+import constants
 
 # приветствуем пользователя и выясняем необходимые данные
 print('Привет, меня зовут FitLife бот')
 
-user_name = input('А как тебя зовут? ')
+user_name = input('А как тебя зовут? ').title()
 print()
-print('Приятно познакомиться,', user_name + '.')
+print(f'Приятно познакомиться, {user_name}.')
 
 print('Мне необходимо узнать некоторые твои параметры,', end=' ')
 print('чтобы правильно составить рекомендации.')
 
-age = int(input('Подскажи, пожалуйста, свой возраст. '))
-print('Отлично! Я могу расчитать индекс массы тела (ИМТ)', end=' ')
-print('и норму воды для тебя.')
+while True:
+    try:
+        age = int(input('Подскажи, пожалуйста, свой возраст. '))
+        print('Отлично! Я могу расчитать индекс массы тела (ИМТ)', end=' ')
+        print('и норму воды для тебя.')
+        break
+    except ValueError:
+        print('Ошибка! Нужно ввести целое число. Попробуй ещё раз.')
 
 print('Для этого мне необходимо узнать твой вес в КГ.')
-user_weight = float(input('Напиши его здесь. '))
-user_height = float(input('Напиши, пожалуйста, мне свой рост в метрах. '))
+user_weight = float(input('Напиши его здесь. ').replace(',', '.'))
+user_height = float(input('Напиши, пожалуйста, мне свой рост в метрах. ').replace(',', '.'))
 
 
 # считаем индекс массы тела и округляем
@@ -25,8 +30,9 @@ bmi = user_weight / (user_height ** 2)
 bmi = round(bmi, 1)
 
 # считаем норму воды в мл
-water_ml = user_weight * WATER_PER_KG
-water_ml = water_ml / 1000  # переводим норму воды в мл в л
+water_ml = user_weight * constants.WATER_PER_KG
+# переводим норму воды в мл в л
+water_ml = water_ml / constants.MILLILITER_IN_LITER
 
 # Выводим данные
 print()
